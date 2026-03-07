cd8c3092a538fc2e9b53265ccfed076903b1b4ff
Kurt Kusch <kurt.kusch@gmail.com>
2026-03-07 13:47:01 -0300

    feat: integrate Vercel Analytics for page views

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 import App from './App';
 
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')!).render(
       }}
     >
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
